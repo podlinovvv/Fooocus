4454ba5f37be3c89ae22ec1395432f02817b2525
podlinovvv <60760208+podlinovvv@users.noreply.github.com>
2024-01-14 12:55:23 +0300

    4444

--- modules/config.py
+++ modules/config.py
@@ -264,8 +264,6 @@ lora_downloads = get_config_item_or_set_default(
     key='lora_downloads',
     default_value={
         "sd_xl_offset_example-lora_1.0.safetensors": "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors",
-        "nsfw_pov.safetensors": "https://civitai.com/api/download/models/160240?type=Model&format=SafeTensor",
-        "nsfw_xl.safetensors": "https://huggingface.co/Dremmar/nsfw-xl/resolve/main/nsfw-xl-2.0.safetensors?download=true"
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
 )
